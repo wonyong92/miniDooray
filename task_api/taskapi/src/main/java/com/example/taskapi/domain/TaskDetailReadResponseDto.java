f8544115e860b647c32521407e4bd0996e6c573b
daecheolsong <eocjf8905@naver.com>
2023-06-11 15:43:55 +0900

    task read 응답 데이터 추가

--- task_api/taskapi/src/main/java/com/example/taskapi/domain/TaskDetailReadResponseDto.java
+++ task_api/taskapi/src/main/java/com/example/taskapi/domain/TaskDetailReadResponseDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -15,6 +16,8 @@ public class TaskDetailReadResponseDto {
     private Integer taskId;
     private String title;
     private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
     private List<TagReadResponseDto> tags;
     private MilestoneReadResponseDto milestone;
     private List<CommentReadResponseDto> comments;
