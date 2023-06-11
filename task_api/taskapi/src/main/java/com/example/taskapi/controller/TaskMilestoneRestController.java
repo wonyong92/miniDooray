160df62d5e3e89a2ad2d3ac903e7bf268b1f7d41
daecheolsong <eocjf8905@naver.com>
2023-06-11 16:10:25 +0900

    TaskMilestone create 응답코드 추가

--- task_api/taskapi/src/main/java/com/example/taskapi/controller/TaskMilestoneRestController.java
+++ task_api/taskapi/src/main/java/com/example/taskapi/controller/TaskMilestoneRestController.java
@@ -5,6 +5,7 @@ import com.example.taskapi.domain.TaskMilestoneCreateDeleteResponseDto;
 import com.example.taskapi.exception.ValidationFailedException;
 import com.example.taskapi.service.TaskMilestoneService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ public class TaskMilestoneRestController {
     private final TaskMilestoneService taskMilestoneService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public TaskMilestoneCreateDeleteResponseDto createTaskMilestone(@RequestBody @Validated TaskMilestoneCreateDeleteRequest taskMilestoneCreateDeleteRequest,
                                                                     BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
