9f308487442107ed8bc055cc485c8e6828765796
daecheolsong <eocjf8905@naver.com>
2023-06-11 15:40:47 +0900

    exception 추가

--- task_api/taskapi/src/main/java/com/example/taskapi/advice/ErrorAdvice.java
+++ task_api/taskapi/src/main/java/com/example/taskapi/advice/ErrorAdvice.java
@@ -1,6 +1,7 @@
 package com.example.taskapi.advice;
 
 import com.example.taskapi.domain.ErrorDto;
+import com.example.taskapi.exception.AlreadyExistedException;
 import com.example.taskapi.exception.NotFoundException;
 import com.example.taskapi.exception.ValidationFailedException;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,11 @@ public class ErrorAdvice {
 
     @ExceptionHandler(value = ValidationFailedException.class)
     public ResponseEntity<ErrorDto> validationFailedExceptionHandle(ValidationFailedException e) {
+        return new ResponseEntity<>(new ErrorDto(String.valueOf(HttpStatus.BAD_REQUEST.value()), e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = AlreadyExistedException.class)
+    public ResponseEntity<ErrorDto> alreadyExistedExceptionHandle(AlreadyExistedException e) {
         return new ResponseEntity<>(new ErrorDto(String.valueOf(HttpStatus.BAD_REQUEST.value()), e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
