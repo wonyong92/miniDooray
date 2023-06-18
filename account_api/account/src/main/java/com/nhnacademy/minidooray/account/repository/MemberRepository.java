496fe7f47b52f87a43eb30a2e2065f28d0cfe27e
장원용 <80381715+wonyong92@users.noreply.github.com>
2023-06-18 21:32:13 +0900

    Delete account_api/account directory

--- account_api/account/src/main/java/com/nhnacademy/minidooray/account/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.minidooray.account.repository;
-
-
-import com.nhnacademy.minidooray.account.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,String> {
-
-}
